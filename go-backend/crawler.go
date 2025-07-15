9eeb46f02d3196a07245820e2acf766cd2ed0d68
Keshav Agrawal <keshavagrawal21@gmail.com>
2025-07-15 14:50:19 +0200

    removed thread sleep

--- go-backend/crawler.go
+++ go-backend/crawler.go
@@ -7,7 +7,6 @@ import (
 	"net/url"
 	"strings"
 	"sync"
-	"time"
 
 	"github.com/gocolly/colly"
 )
@@ -128,9 +127,6 @@ func crawl(currenturl string, id int64) Response {
 	})
 
 	c.Request("GET", currenturl, nil, collyCtx, nil)
-
-	//The below line is just for testing will be removed completely later
-	time.Sleep(10 * time.Second)
 
 	checker.Wait()
 	c.Wait()
